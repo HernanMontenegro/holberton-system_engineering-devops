f1bf6198c2ce716a2304a48bbd11c49ec397b6d1
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-22 00:25:04 +0000

    a

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,37 +4,48 @@
 
 int infinite_while(void);
 
+/**
+ * main - create 5 zombie processes
+ * ---------------------------------
+ * Return: 0 if worked, 1 if not
+*/
 int main(void)
 {
-    pid_t child_pid;
-    int i;
+	pid_t child_pid;
+	int i;
 
-    for (i = 1; i <= 5; i++)
-    {
-        child_pid = fork();
-        if (child_pid == -1)
-        {
-            perror("Fork failed");
-            return (0);
-        }
-        if (child_pid == 0)
-        {
-            pause();
-        }
-        else
-        {
-            printf("Zombie process created, PID: %d\n", child_pid);
-        }
-    }
-    infinite_while();
-	return (1);
+	for (i = 1; i <= 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Fork failed");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			pause();
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+		}
+	}
+	infinite_while();
+
+	return (0);
 }
 
+/**
+ * infinite_while - an infinite loop given by holberton
+ * -----------------------------------
+ * Return: 0 because yes
+*/
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
