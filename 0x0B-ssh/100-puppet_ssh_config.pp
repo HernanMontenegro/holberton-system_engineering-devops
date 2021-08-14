c4a9e65958975f79c585ad193ddc3df92f93c017
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-14 15:13:33 +0000

    100 done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+# Doing config with Puppet
+
+file_line { 'change private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => 'IdentityFile ~/.ssh/id_rsa',
+}
+
+file_line { 'no password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => 'PasswordAuthentication yes',
+  line   => 'PasswordAuthentication no',
+}
