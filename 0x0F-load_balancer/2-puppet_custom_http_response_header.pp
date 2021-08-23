b044796b6c640dbe71de26f859ca76e9faacf8fc
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-23 18:29:21 +0000

    ?

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,9 @@
 # Add new header with puppet
 
 exec { 'add X-Served-By header':
-    command => 'sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/sites-available/default;
+    command => 'sudo apt-get update;
+                sudo apt-get install nginx;
+                sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/sites-available/default;
                 sudo service nginx restart',
     provider => 'shell',
 }
