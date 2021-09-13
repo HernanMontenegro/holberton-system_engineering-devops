dee5b434cd2f990e49b0d1c2f344c3ed1868c7a3
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-13 16:45:56 -0300

    2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,7 +10,7 @@ from sys import argv as av
 if (__name__ == '__main__'):
     url = 'https://jsonplaceholder.typicode.com/todos?userId=' + av[1]
     t = req.get(url)
-    u = req.get(f'https://jsonplaceholder.typicode.com/users?id=' + av[1])
+    u = req.get('https://jsonplaceholder.typicode.com/users?id=' + av[1])
 
     taskList = t.json()
     userDb = u.json()[0]
@@ -26,5 +26,5 @@ if (__name__ == '__main__'):
 
     json_dict[av[1]] = aux_list
 
-    with open(f'{av[1]}.json', 'w') as f:
+    with open('{}.json'.format(av[1]), 'w') as f:
         json.dump(json_dict, f)
