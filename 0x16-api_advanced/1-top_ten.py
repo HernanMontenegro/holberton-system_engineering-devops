192c696d054929398337f4b241ff41e03428a365
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-28 15:40:27 -0300

    1 top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,13 +5,13 @@ import requests as req
 
 def top_ten(subreddit):
     '''prints the titles of the first 10 hot posts for a given subreddit'''
-    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
     reddit = req.get(url, headers={'User-agent': 'your bot 0.1'})
 
     if (reddit.status_code >= 200 and reddit.status_code <= 299):
         reddict = reddit.json()
         childrenList = reddict['data']['children']
-        for i in range(0, 10):
+        for i in range(0, len(childrenList)):
             print(childrenList[i]['data']['title'])
         return
     return print('None')
